a4a4b4493e9f1712780edd39334fbb3377360423
luisminerao <00001088202329sp@al.educacao.sp.gov.br>
2024-11-29 20:20:52 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,34 +1,34 @@
 criaCartao(
     'Tema CSS',
-    ' O que é CSS?',
+    ' O que é CSS?'
     'É uma linguagem para estilizar páginas HTML'
 )
 
 criaCartao(
     'Tema CSS',
-    'Para que serve o id no CSS?',
+    'Para que serve o id no CSS?'
     'Estilizar um elemento único'
 )
 
 criaCartao(
     'Tema CSS',
-    ' Como mudar a cor de fundo com CSS?',
+    ' Como mudar a cor de fundo com CSS?'
     'Use background-color'
 )
 
 criaCartao(
     'Tema HTML',
-    'Para que serve a tag <a>?',
+    'Para que serve a tag <a>?'
     'Criar links'
 )
 criaCartao(
     'Tema HTML',
-    'O que é uma tag <div>?',
+    'O que é uma tag <div>?'
     'Um contêiner de bloco para agrupar elementos'
 )
 criaCartao(
     'Tema HTML',
-    'Como criar uma lista numerada?',
+    'Como criar uma lista numerada?'
     'Use a tag <ol> com <li>'
 )
 criaCartao(
